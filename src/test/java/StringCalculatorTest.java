02cf83d6daa40be3bd3d7510987ab8a3e6087409
bjarki15 <bjarki15@ru.is>
2017-10-10 19:29:58 +0000

    testing 1 as number input

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -7,5 +7,7 @@ public class StringCalculatorTest {
 	@Test public void testSum() {
 		StringCalculator Test = new StringCalculator();
         assertEquals(0, Test.add(" "));
+		assertEquals(1, Test.add("1"));
     }
+	
 }
