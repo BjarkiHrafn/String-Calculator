f194b26e7f79a89d3ab58e94c543db4e562434ec
bjarki15 <bjarki15@ru.is>
2017-10-10 19:59:15 +0000

    testing not equals

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -10,6 +10,7 @@ public class StringCalculatorTest {
 		assertEquals(1, Test.add("1"));
 		assertEquals(12, Test.add("2,5,5"));
 		assertEquals(42, Test.add("2,-43,73,-2,5,7"));
+		assertNotEquals(100, Test.add("3,4,5,6,3"));
     }
 	
 }
