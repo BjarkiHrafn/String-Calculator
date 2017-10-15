0475738b97efa80555f01ddf2fcf9267e8b77808
bjarki15 <bjarki15@ru.is>
2017-10-15 11:30:12 +0000

    Numbers bigger than 1000 tested

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,24 +1,36 @@
 public class StringCalculator{
 
 	public StringCalculator(){
-	
+
 	}
-	
+
 	public int add(String numbers){
-		
+
+		String[] strArray = numbers.split(",|\n");
+
 		if(numbers == " "){
 			return 0;
 		}
+
 		int sum = 0;
-		String[] strArray = numbers.split(",|\n");
+		String denied = "";
 		int[] intArray = new int[strArray.length];
-			
+
 			for(int i = 0; i < strArray.length; i++) {
-				
+
 				intArray[i] = Integer.parseInt(strArray[i]);
+				if(intArray[i] < 1000)
 				sum += intArray[i];
+				if(intArray[i] < 0){
+                    denied  += intArray[i] + ",";
+				}
 			}
-			
+
+			if(!denied.isEmpty()){
+               throw new RuntimeException("Negatives not allowed: " + denied);
+			}
+
+
 			return sum;
 	}
 
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -3,16 +3,36 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
-    
+
 	@Test public void testSum() {
 		StringCalculator Test = new StringCalculator();
         assertEquals(0, Test.add(" "));
 		assertEquals(1, Test.add("1"));
 		assertEquals(12, Test.add("2,5,5"));
-		assertEquals(42, Test.add("2,-43,73,-2,5,7"));
+
 		assertNotEquals(100, Test.add("3,4,5,6,3"));
 		//assertEquals(100, Test.add("3,4,5,6,y,3"));
 		assertEquals(6, Test.add("1\n2,3"));
     }
-	
+    @Test public void testNeg(){
+        StringCalculator Test = new StringCalculator();
+        RuntimeException exception = null;
+
+        try {
+            Test.add("3,-6,15,-18,46,33");
+        } catch (RuntimeException e) {
+            exception = e;
+        }
+            assertNotNull(exception);
+           // assertEquals("Negatives not allowed: [-6,-15,-18]", exception.getMessage());
+    }
+
+    @Test public void tooBig(){
+
+        StringCalculator Test = new StringCalculator();
+
+        assertEquals(32, Test.add("32,1001"));
+
+    }
+
 }
